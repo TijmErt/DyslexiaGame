219037641609aeedbdf4b835eed09b8a03b909c9
Q4eenfreak <sammiev06@gmail.com>
2026-03-20 15:24:37 +0100

    EOD - Word gets properly placed

---
+++ ProjectKasteelBoekendal/Assets/Scripts/Word Split/AnswerCheck.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AnswerCheck : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void WrongAnswer()
+    {
+        Debug.Log("Wrong Answer");
+    }
+
+    void CorrectAnswer()
+    {
+        Debug.Log("Correct Answer");
+    }
+}
--- ProjectKasteelBoekendal/Assets/Scripts/Word Split/WordFormer.cs
+++ ProjectKasteelBoekendal/Assets/Scripts/Word Split/WordFormer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class WordFormer : MonoBehaviour
 {
@@ -7,7 +8,15 @@ public class WordFormer : MonoBehaviour
     List<string> wordParts;
     List<List<char>> lettersPerSyllable;
 
-   
+    public GameObject letterPrefab;
+    public GameObject slicePrefab;
+    public Transform canvas;
+
+    public GameObject textMeshObj;
+
+    float xOffset = 0;
+    float spacing = 30f;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,6 +47,17 @@ public class WordFormer : MonoBehaviour
                 // Spawn letter
                 Debug.Log(letter);
 
+                // Get the TextMeshPro component and set the letter
+                TextMeshProUGUI textMesh = textMeshObj.GetComponentInChildren<TextMeshProUGUI>();
+                string character = letter.ToString();
+                textMesh.text = character;
+
+                GameObject letterObj = Instantiate(letterPrefab, canvas);
+
+                RectTransform letterRect = letterObj.GetComponent<RectTransform>();
+                letterRect.anchoredPosition = new Vector2(xOffset, 0);
+
+                xOffset += spacing;
 
                 if (i == wordParts.Count - 1 && j == syllable.Length - 1)
                 {
@@ -45,11 +65,17 @@ public class WordFormer : MonoBehaviour
                 }
                 else if (j == syllable.Length - 1)
                 {
-                    Debug.Log("Yay");
+                    GameObject sliceObj = Instantiate(slicePrefab, canvas);
+                    RectTransform sliceRect = sliceObj.GetComponent<RectTransform>();
+                    sliceRect.anchoredPosition = new Vector2(xOffset, 0);
+                    xOffset += spacing;
                 }
                 else
                 {
-                    Debug.Log("Nay");
+                    GameObject sliceObj = Instantiate(slicePrefab, canvas);
+                    RectTransform sliceRect = sliceObj.GetComponent<RectTransform>();
+                    sliceRect.anchoredPosition = new Vector2(xOffset, 0);
+                    xOffset += spacing;
                 }
             }   
         }

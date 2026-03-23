b114332831af5c527f475f5fd5358735ea38e779
Q4eenfreak <sammiev06@gmail.com>
2026-03-23 14:34:28 +0100

    EOD added win and lose

--- ProjectKasteelBoekendal/Assets/Scripts/Word Split/AnswerCheck.cs
+++ ProjectKasteelBoekendal/Assets/Scripts/Word Split/AnswerCheck.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class AnswerCheck : MonoBehaviour
 {
+    public GameObject slicePrefab;
+    public WordFormer wordFormer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,13 +19,17 @@ public class AnswerCheck : MonoBehaviour
         
     }
 
-    void WrongAnswer()
-    {
-        Debug.Log("Wrong Answer");
-    }
-
-    void CorrectAnswer()
+    public void CorrectAnswer()
     {
         Debug.Log("Correct Answer");
+        wordFormer.SplitWord();
+        wordFormer.RightWord();
+
+    }
+
+    public void WrongAnswer()
+    {
+        Debug.Log("Wrong Answer");
+        wordFormer.WrongWord();
     }
 }
--- ProjectKasteelBoekendal/Assets/Scripts/Word Split/WordFormer.cs
+++ ProjectKasteelBoekendal/Assets/Scripts/Word Split/WordFormer.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Collections;
 using TMPro;
 
 public class WordFormer : MonoBehaviour
@@ -8,15 +9,22 @@ public class WordFormer : MonoBehaviour
     List<string> bomen;
     List<string> wordParts;
     List<List<char>> lettersPerSyllable;
+    List<GameObject> buttons;
+    List<GameObject> letters;
 
     public GameObject letterPrefab;
     public GameObject slicePrefab;
     public Transform canvas;
 
     public GameObject textMeshObj;
+    public AnswerCheck answerCheck;
 
     float xOffset = 0;
     float spacing = 30f;
+    float duration = 5f;
+
+    public Color oldButtoncolor;
+    public Color oldLetterColor;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -24,6 +32,8 @@ public class WordFormer : MonoBehaviour
     {
         bomen = new List<string>{"bo", "men"};
         wordParts = bomen;
+        buttons = new List<GameObject>();
+        letters = new List<GameObject>();
         FormWord();
     }
 
@@ -54,6 +64,7 @@ public class WordFormer : MonoBehaviour
                 textMesh.text = character;
 
                 GameObject letterObj = Instantiate(letterPrefab, canvas);
+                letters.Add(letterObj);
 
                 RectTransform letterRect = letterObj.GetComponent<RectTransform>();
                 letterRect.anchoredPosition = new Vector2(xOffset, 0);
@@ -67,18 +78,115 @@ public class WordFormer : MonoBehaviour
                 else if (j == syllable.Length - 1)
                 {
                     GameObject sliceObj = Instantiate(slicePrefab, canvas);
+                    buttons.Add(sliceObj);
                     RectTransform sliceRect = sliceObj.GetComponent<RectTransform>();
                     sliceRect.anchoredPosition = new Vector2(xOffset, 0);
                     xOffset += spacing;
+                    sliceObj.GetComponent<Button>().onClick.AddListener(answerCheck.CorrectAnswer);
                 }
                 else
                 {
                     GameObject sliceObj = Instantiate(slicePrefab, canvas);
+                    buttons.Add(sliceObj);
                     RectTransform sliceRect = sliceObj.GetComponent<RectTransform>();
                     sliceRect.anchoredPosition = new Vector2(xOffset, 0);
                     xOffset += spacing;
+                    sliceObj.GetComponent<Button>().onClick.AddListener(answerCheck.WrongAnswer);
                 }
             }   
         }
+    }
+
+    public void SplitWord()
+    {
+        HidePreviousPrefabs();
+
+        xOffset = 0;
+
+        for (int i = 0; i < wordParts.Count; i++)
+        {
+            string syllable = wordParts[i];
+
+            TextMeshProUGUI textMesh = textMeshObj.GetComponentInChildren<TextMeshProUGUI>();
+            textMesh.text = syllable;
+            
+            GameObject syllableObj = Instantiate(letterPrefab, canvas);
+
+            RectTransform syllableRect = syllableObj.GetComponent<RectTransform>();
+            syllableRect.anchoredPosition = new Vector2(xOffset, 0);
+
+            xOffset += 2*spacing;
+        }
+    }
+    public void HidePreviousPrefabs()
+    {
+        foreach(GameObject button in buttons)
+        {
+            Destroy(button);
+        }
+        buttons.Clear();
+
+        foreach(GameObject letter in letters)
+        {
+            Destroy(letter);
+        }
+        letters.Clear();
+    }
+
+    public void WrongWord()
+    {
+        foreach(GameObject button in buttons)
+        {
+            Image buttonImage = button.GetComponent<Image>();
+            if (buttonImage != null)
+            {
+                buttonImage.color = Color.red;
+                StartCoroutine(SwitchColorBackButton(buttonImage));
+            }
+        }
+        buttons.Clear();
+        foreach(GameObject letter in letters)
+        {
+            Image letterImage = letter.GetComponent<Image>();
+            if (letterImage != null)
+            {
+                letterImage.color = Color.red;
+                StartCoroutine(SwitchColorBackLetter(letterImage));
+            }
+        }
+        letters.Clear();
+
+    }
+    public void RightWord()
+    {
+        foreach(GameObject button in buttons)
+        {
+            Image buttonImage = button.GetComponent<Image>();
+            if (buttonImage != null)
+            {
+                buttonImage.color = Color.green;
+            }
+        }
+        buttons.Clear();
+
+        foreach(GameObject letter in letters)
+        {
+            Image letterImage = letter.GetComponent<Image>();
+            if (letterImage != null)
+            {
+                letterImage.color = Color.green;
+            }
+        }
+        letters.Clear();
+    }
+    private IEnumerator SwitchColorBackButton(Image image)
+    {
+        yield return new WaitForSeconds(duration);
+        image.color = Color.white;
+    }
+    private IEnumerator SwitchColorBackLetter(Image image)
+    {
+        yield return new WaitForSeconds(duration);
+        image.color = Color.white;
     }
 }

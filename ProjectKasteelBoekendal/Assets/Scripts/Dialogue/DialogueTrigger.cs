dd3682c2c93acaa35564fbe2f4ab0a671a5f0a3c
TijmErt <95760062+TijmErt@users.noreply.github.com>
2026-06-05 08:03:29 +0200

    Updated Dialogue Trigger to respond temporarily to the "E" key when in range of collider

--- ProjectKasteelBoekendal/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ ProjectKasteelBoekendal/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -1,5 +1,7 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 /// <summary>
@@ -9,7 +11,7 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
 {
 	[field: SerializeField] public float InteractionDistance { get; set; } = 2f;
 	[field: SerializeField] public GameObject DialogueOverlay { get; set; }
-
+	private bool playerInRange;
 	public Vector3 GetPlayerPosPoint(PlayerInteraction player)
 	{
 		return transform.position;
@@ -19,5 +21,26 @@ public class DialogueTrigger : MonoBehaviour, IInteractable
 	{
 		// Trigger dialogue to start
 		EventBus.Trigger(new EventHook("OnDialogueStart", this.gameObject));
+	}
+	private void Update()
+	{
+		if (!playerInRange)
+			return;
+
+		if (Keyboard.current.eKey.wasPressedThisFrame)
+		{
+			EventBus.Trigger(new EventHook("OnDialogueStart", gameObject));
+		}
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player1"))
+			playerInRange = true;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player1"))
+			playerInRange = false;
 	}
 }

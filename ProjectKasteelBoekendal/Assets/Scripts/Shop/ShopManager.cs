cc080f4d83d81b48ab0f9dcb4feff6da4d341152
Q4eenfreak <sammiev06@gmail.com>
2026-06-10 11:30:35 +0200

    EOD - Started shop UI

---
+++ ProjectKasteelBoekendal/Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ShopManager : MonoBehaviour
+{
+    public CurrencyMediator currencyMediator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
